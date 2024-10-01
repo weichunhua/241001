5b480ed671433ed0c4668ee48fbf886653a999ec
weichunhua <944695908@qq.com>
2024-10-01 13:39:07 +0800

    默认变更列表

---
+++ src/main/java/com/example/rabbitmqtemplet/RabbitmqTempletApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqtemplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTempletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTempletApplication.class, args);
+    }
+
+}
